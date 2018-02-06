e16f081f38a5c459850aaffc1503fdd1579c8961
John <jdavidswift@gmail.com>
2018-02-05 18:55:16 -0600

    Basic information for react
    Must load react into this

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+class Tomato extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  return{
+    render(
+      <div>
+      <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
 
+ReactDOM.render(<Tomato />, document.getElementById('myDiv'));
